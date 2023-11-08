430d1fe7e702c69a3b0b866777cfa87f787b08f8
hokki <duaghrud22@gmail.com>
2023-11-08 20:17:14 +0900

    11/8/night

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:team_husky/firebase_options.dart';
 import 'package:team_husky/user/user_screen.dart';
 import 'package:team_husky/view/main_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
