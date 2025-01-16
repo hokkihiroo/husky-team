92c026e4a8f91ba42249ff3f1b09d0dd3efad0fc
hokki <duaghrud22@gmail.com>
2025-01-16 18:10:33 +0900

    01/16/afternoon

--- lib/5mypage/mysalary/mySalary.dart
+++ lib/5mypage/mysalary/mySalary.dart
@@ -68,6 +68,8 @@ class _MySalaryState extends State<MySalary> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width; // 화면의 너비
+    final screenHeight = MediaQuery.of(context).size.height; // 화면의 높이
     String formattedAmount = NumberFormat('#,###').format(amount);
 
     return Scaffold(
@@ -107,9 +109,9 @@ class _MySalaryState extends State<MySalary> {
       body: Container(
         child: Center(
           child: Container(
-            color: Colors.white,
-            height: 600,
-            width: 380,
+            color: Colors.blueGrey,
+            height: screenHeight * 0.8,
+            width: screenWidth * 0.9,
             child: SingleChildScrollView(
               child: Column(
                 children: [
